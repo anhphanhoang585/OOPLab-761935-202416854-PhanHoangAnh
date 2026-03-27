14198e5f329f4bd39f45aed5bd42c2cce92b09a2
anhphanhoang585 <phanhoanganh2903@gmail.com>
2026-03-27 13:46:41 +0700

    update

--- Lab_01/HelloNameDialog.java
+++ Lab_01/HelloNameDialog.java
@@ -2,7 +2,6 @@
 import javax.swing.JOptionPane;
 
 public class HelloNameDialog {
-    
     public static void main(String[] args) {
         String result;
         result = JOptionPane.showInputDialog("Please enter your name:");
